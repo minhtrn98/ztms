9a2b8c26cdbd82fa06a03015b1049d1d3547e2d8
minhtrn98 <minh.tran.cong@247express.vn>
2026-08-05 17:25:52 +0700

    Fix color definitions for menu items in MenuTheme.psm1

--- modules/MenuTheme.psm1
+++ modules/MenuTheme.psm1
@@ -4,9 +4,9 @@
 $MenuTheme = [ordered]@{
     Cursor         = "Yellow"     # currently highlighted row
     Checked        = "Green"    # checkbox item toggled on
-    Normal         = "DarkGray"     # unselected / unchecked item
+    Normal         = "Gray"     # unselected / unchecked item
     HeaderActive   = "DarkYellow" # group header the cursor is currently inside
-    HeaderInactive = "Gray" # other, non-active group headers
+    HeaderInactive = "DarkGray" # other, non-active group headers
     Hint           = "Cyan" # instructional line under the prompt
     Prompt         = "Cyan"     # Confirm-Prompt question text
     Error          = "Red"      # cancelled / no-entries messages
